8886d21c9f5ed084a35185ade37db6e7630bff93
Holli90 <109025573+Holli90@users.noreply.github.com>
2024-09-12 08:28:29 +0700

    add

---
+++ Assets/Resources/Shaders/ToonRamp.hlsl
@@ -0,0 +1,14 @@
+﻿#ifdef SHADERGRAPH_PREVIEW
+ToonRampOutput = float3(0.5,0.5,0);
+Direction = float3(0.5,0.5,0);
+#else
+#if SHADOWS_SCREEN
+half4 shadowCoord = ComputeScreenPos(ClipSpacePos);
+#else
+half4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+#endif 
+#if _MAIN_LIGHT_SHADOWS_CASCADE || _MAIN_LIGHT_SHADOWS
+Light light = GetMainLight(shadowCoord);
+#else
+Light light = GetMainLight();
+#endif
